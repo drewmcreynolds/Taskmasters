fe49489d46882e3c7b29a4f5ced920e8c5bc6cb9
drewmcreynolds <drew.gmcreynolds@gmail.com>
2021-09-07 09:17:54 -0600

    alert

--- app/Controllers/ListsController.js
+++ app/Controllers/ListsController.js
@@ -29,7 +29,20 @@ export class ListsController {
 
   deleteList(listId){
     listsService.deleteList(listId)
-    
+    Swal.fire({
+      title: 'Do you want to save the changes?',
+      showDenyButton: true,
+      showCancelButton: true,
+      confirmButtonText: 'Save',
+      denyButtonText: `Don't save`,
+    }).then((result) => {
+      /* Read more about isConfirmed, isDenied below */
+      if (result.isConfirmed) {
+        Swal.fire('Saved!', '', 'success')
+      } else if (result.isDenied) {
+        Swal.fire('Changes are not saved', '', 'info')
+      }
+    })
   }
   
 }
